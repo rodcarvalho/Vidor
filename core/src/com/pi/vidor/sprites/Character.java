7780d80db81e81d9a7cdb5155b485901f8fa014e
rodcarvalho <barretocarvalho.contato@gmail.com>
2016-05-10 22:29:21 -0300

    Boas Praticas
    Conversão da classe Character para boas práticas. Criação da classe do
    inimigo Wolf

--- core/src/com/pi/vidor/sprites/Character.java
+++ core/src/com/pi/vidor/sprites/Character.java
@@ -7,6 +7,7 @@ package com.pi.vidor.sprites;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -21,7 +22,9 @@ import com.pi.vidor.Main;
  */
 public abstract class Character extends Sprite {
     private enum State {STANDING, RUNNING, ATTACKING};
-    
+    private State current_state;
+    private State previous_state;
+
     private World world;
     private Body b2body;
     
@@ -33,7 +36,64 @@ public abstract class Character extends Sprite {
     private TextureRegion texture;
     
     public abstract void defineBody();
-    
-    
-    
+
+    public World getWorld() {
+        return world;
+    }
+
+    public void setWorld(World world) {
+        this.world = world;
+    }
+
+    public Body getB2body() {
+        return b2body;
+    }
+
+    public void setB2body(Body b2body) {
+        this.b2body = b2body;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public State getCurrent_state() {
+        return current_state;
+    }
+
+    public void setCurrent_state() {
+        this.current_state = current_state;
+    }
+
+    public State getPrevious_state() {
+        return previous_state;
+    }
+
+    public void setPrevious_state() {
+        this.previous_state = previous_state;
+    }
+
+    public Animation getRun() {
+        return run;
+    }
+
+    public void setRun(Animation run) {
+        this.run = run;
+    }
+
+    public Animation getAttack() {
+        return attack;
+    }
+
+    public void setAttack(Animation attack) {
+        this.attack = attack;
+    }
+
+    public void setTexture(TextureRegion texture) {
+        this.texture = texture;
+    }
 }
